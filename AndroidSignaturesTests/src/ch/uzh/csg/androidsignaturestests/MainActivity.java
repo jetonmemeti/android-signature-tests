43c587ee3471474600f9e86aba24a299e63d12a9
Jeton Memeti <jeton.memeti@gmail.com>
2014-05-10 23:01:56 +0200

    Changed measurement type.

--- AndroidSignaturesTests/src/ch/uzh/csg/androidsignaturestests/MainActivity.java
+++ AndroidSignaturesTests/src/ch/uzh/csg/androidsignaturestests/MainActivity.java
@@ -2,7 +2,6 @@ package ch.uzh.csg.androidsignaturestests;
 
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
-import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -22,11 +21,9 @@ import android.view.ViewGroup;
 
 public class MainActivity extends Activity {
 	
+	private static final String TAG = "MainActivity";
 	private static final String PLAIN_TEXT = "lorem ipsum blablabla";
 	
-	private static PrivateKey key1024;
-	private static PrivateKey key2048;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,169 +33,241 @@ public class MainActivity extends Activity {
 			getFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
 		}
 		
-		KeyPairGenerator keyGen = null;
-		try {
-			keyGen = KeyPairGenerator.getInstance("RSA");
-		} catch (NoSuchAlgorithmException e) {
-			Log.e("MainActivity", "error", e);
-			System.exit(0);
-		}
-		
-		key1024 = keyGen.generateKeyPair().getPrivate();
-		
-		keyGen.initialize(2048);
-		key2048 = keyGen.generateKeyPair().getPrivate();
-		
 		measureCreate1024Keys();
 		measureCreate2048Keys();
 		
-		measureRunTime1();
-		measureRunTime2();
-		measureRunTime3();
-		measureRunTime4();
+		measureSHA1withRSA1024();
+		measureSHA1withRSA2048();
+		measureSHA256withRSA1024();
+		measureSHA256withRSA2048();
 	}
 	
 	private static void measureCreate1024Keys() {
-		long start = System.currentTimeMillis();
+		Log.d(TAG, "start benchmark - RSA-1024");
+		
+		StringBuilder builder = new StringBuilder();
+		
 		for (int i=0; i<10; i++) {
 			try {
-				create1024Keys();
+				if (i > 0)
+					builder.append(", ");
+				
+				builder.append(create1024Keys());
 			} catch (NoSuchAlgorithmException e) {
-				Log.e("MainActivity", "error", e);
+				Log.e(TAG, "error", e);
 				break;
 			}
 		}
-		long end = System.currentTimeMillis();
-		Log.d("MainActivity", "SHA1-RSA1024, total duration in ms: "+(end-start));
+		
+		Log.d(TAG, builder.toString());
+		Log.d(TAG, "finished benchmark - RSA-1024");
 	}
 	
-	private static void create1024Keys() throws NoSuchAlgorithmException {
+	private static long create1024Keys() throws NoSuchAlgorithmException {
+		long start = System.currentTimeMillis();
 		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-		KeyPair keyPair = keyGen.generateKeyPair();
+		keyGen.generateKeyPair();
+		return (start - System.currentTimeMillis());
 	}
 	
 	private static void measureCreate2048Keys() {
-		long start = System.currentTimeMillis();
+		Log.d(TAG, "start benchmark - RSA-2048");
+		
+		StringBuilder builder = new StringBuilder();
+		
 		for (int i=0; i<10; i++) {
 			try {
-				create2048Keys();
+				if (i > 0)
+					builder.append(", ");
+				
+				builder.append(create2048Keys());
 			} catch (NoSuchAlgorithmException e) {
-				Log.e("MainActivity", "error", e);
+				Log.e(TAG, "error", e);
 				break;
 			}
 		}
-		long end = System.currentTimeMillis();
-		Log.d("MainActivity", "SHA1-RSA1024, total duration in ms: "+(end-start));
+		
+		Log.d(TAG, builder.toString());
+		Log.d(TAG, "finished benchmark - RSA-2048");
 	}
-
-	private static void create2048Keys() throws NoSuchAlgorithmException {
+	
+	private static long create2048Keys() throws NoSuchAlgorithmException {
+		long start = System.currentTimeMillis();
 		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
 		keyGen.initialize(2048);
-		KeyPair keyPair = keyGen.generateKeyPair();		
+		keyGen.generateKeyPair();		
+		return (start - System.currentTimeMillis());
 	}
-
-	private static void measureRunTime1() {
-		long start = System.currentTimeMillis();
+	
+	private static void measureSHA1withRSA1024() {
+		Log.d(TAG, "start benchmark - SHA1withRSA1024");
+		
+		PrivateKey privateKey = generateNewKey(1024);
+		
+		StringBuilder builder = new StringBuilder();
+		
 		for (int i=0; i<10; i++) {
 			try {
-				SHA1withRSA(key1024);
+				if (i > 0)
+					builder.append(", ");
+				
+				builder.append(SHA1withRSA(privateKey));
 			} catch (Exception e) {
-				Log.e("MainActivity", "error", e);
+				Log.e(TAG, "error", e);
 				break;
 			}
 		}
-		long end = System.currentTimeMillis();
-		Log.d("MainActivity", "SHA1-RSA1024, total duration in ms: "+(end-start));
+		
+		Log.d(TAG, builder.toString());
+		Log.d(TAG, "finished benchmark - SHA1withRSA1024");
 	}
 	
-	private static void measureRunTime2() {
+	private static long SHA1withRSA(PrivateKey privateKey) throws NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException, SignatureException{
 		long start = System.currentTimeMillis();
-		for (int i=0; i<10; i++) {
-			try {
-				SHA1withRSA2048(key2048);
-			} catch (Exception e) {
-				Log.e("MainActivity", "error", e);
-				break;
-			}
-		}
-		long end = System.currentTimeMillis();
-		Log.e("MainActivity", "SHA1-RSA2048, total duration in ms: "+(end-start));
-	}
-	
-	private static void measureRunTime3() {
-		long start = System.currentTimeMillis();
-		for (int i=0; i<10; i++) {
-			try {
-				SHA256withRSA(key1024);
-			} catch (Exception e) {
-				Log.e("MainActivity", "error", e);
-				break;
-			}
-		}
-		long end = System.currentTimeMillis();
-		Log.e("MainActivity", "SHA256-RSA1024, total duration in ms: "+(end-start));
-	}
-	
-	private static void measureRunTime4() {
-		long start = System.currentTimeMillis();
-		for (int i=0; i<10; i++) {
-			try {
-				SHA256withRSA2048(key2048);
-			} catch (Exception e) {
-				Log.e("MainActivity", "error", e);
-				break;
-			}
-		}
-		long end = System.currentTimeMillis();
-		Log.e("MainActivity", "SHA256-RSA2048, total duration in ms: "+(end-start));
-	}
-	
-	private static void SHA1withRSA(PrivateKey privateKey) throws NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException, SignatureException{
+		
 		MessageDigest digest = MessageDigest.getInstance("SHA-1");
 		byte[] digest2 = digest.digest(PLAIN_TEXT.getBytes("UTF-8"));
-		System.out.println("size of digest: "+digest2.length);
 		
 		Signature rsa = Signature.getInstance("SHA1withRSA");
 		rsa.initSign(privateKey);
 		rsa.update(digest2);
-		byte[] sign = rsa.sign();
-		System.out.println("size of the signature: "+sign.length);
-	}
-	
-	private static void SHA256withRSA(PrivateKey privateKey) throws NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException, SignatureException{
-		MessageDigest digest = MessageDigest.getInstance("SHA-256");
-		byte[] digest2 = digest.digest(PLAIN_TEXT.getBytes("UTF-8"));
-		System.out.println("size of digest: "+digest2.length);
+		rsa.sign();
 		
-		Signature rsa = Signature.getInstance("SHA256withRSA");
-		rsa.initSign(privateKey);
-		rsa.update(digest2);
-		byte[] sign = rsa.sign();
-		System.out.println("size of the signature: "+sign.length);
+		return (start - System.currentTimeMillis());
 	}
 	
-	private static void SHA1withRSA2048(PrivateKey privateKey) throws NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException, SignatureException{
+	private static void measureSHA1withRSA2048() {
+		Log.d(TAG, "start benchmark - SHA1withRSA2048");
+		
+		PrivateKey privateKey = generateNewKey(2048);
+		
+		StringBuilder builder = new StringBuilder();
+		
+		for (int i=0; i<10; i++) {
+			try {
+				if (i > 0)
+					builder.append(", ");
+				
+				builder.append(SHA1withRSA2048(privateKey));
+			} catch (Exception e) {
+				Log.e(TAG, "error", e);
+				break;
+			}
+		}
+		
+		Log.d(TAG, builder.toString());
+		Log.d(TAG, "finished benchmark - SHA1withRSA2048");
+	}
+	
+	private static long SHA1withRSA2048(PrivateKey privateKey) throws NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException, SignatureException{
+		long start = System.currentTimeMillis();
+		
 		MessageDigest digest = MessageDigest.getInstance("SHA-1");
 		byte[] digest2 = digest.digest(PLAIN_TEXT.getBytes("UTF-8"));
-		System.out.println("size of digest: "+digest2.length);
 		
 		Signature rsa = Signature.getInstance("SHA1withRSA");
 		rsa.initSign(privateKey);
 		rsa.update(digest2);
-		byte[] sign = rsa.sign();
-		System.out.println("size of the signature: "+sign.length);
+		rsa.sign();
+		
+		return (start - System.currentTimeMillis());
 	}
 	
-	private static void SHA256withRSA2048(PrivateKey privateKey) throws NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException, SignatureException{
+	private static void measureSHA256withRSA1024() {
+		Log.d(TAG, "start benchmark - SHA256withRSA1024");
+		
+		PrivateKey privateKey = generateNewKey(1024);
+		
+		StringBuilder builder = new StringBuilder();
+		
+		for (int i=0; i<10; i++) {
+			try {
+				if (i > 0)
+					builder.append(", ");
+				
+				builder.append(SHA256withRSA1024(privateKey));
+			} catch (Exception e) {
+				Log.e(TAG, "error", e);
+				break;
+			}
+		}
+		
+		Log.d(TAG, builder.toString());
+		Log.d(TAG, "finished benchmark - SHA256withRSA1024");
+	}
+	
+	private static long SHA256withRSA1024(PrivateKey privateKey) throws NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException, SignatureException{
+		long start = System.currentTimeMillis();
+		
 		MessageDigest digest = MessageDigest.getInstance("SHA-256");
 		byte[] digest2 = digest.digest(PLAIN_TEXT.getBytes("UTF-8"));
-		System.out.println("size of digest: "+digest2.length);
 		
 		Signature rsa = Signature.getInstance("SHA256withRSA");
 		rsa.initSign(privateKey);
 		rsa.update(digest2);
-		byte[] sign = rsa.sign();
-		System.out.println("size of the signature: "+sign.length);
+		rsa.sign();
+		
+		return (start - System.currentTimeMillis());
+	}
+	
+	private static void measureSHA256withRSA2048() {
+		Log.d(TAG, "start benchmark - SHA256withRSA2048");
+		
+		PrivateKey privateKey = generateNewKey(2048);
+		
+		StringBuilder builder = new StringBuilder();
+		
+		for (int i=0; i<10; i++) {
+			try {
+				if (i > 0)
+					builder.append(", ");
+				
+				builder.append(SHA256withRSA2048(privateKey));
+			} catch (Exception e) {
+				Log.e(TAG, "error", e);
+				break;
+			}
+		}
+		
+		Log.d(TAG, builder.toString());
+		Log.d(TAG, "finished benchmark - SHA256withRSA1024");
+	}
+	
+	private static long SHA256withRSA2048(PrivateKey privateKey) throws NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException, SignatureException{
+		long start = System.currentTimeMillis();
+		
+		MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		byte[] digest2 = digest.digest(PLAIN_TEXT.getBytes("UTF-8"));
+		
+		Signature rsa = Signature.getInstance("SHA256withRSA");
+		rsa.initSign(privateKey);
+		rsa.update(digest2);
+		rsa.sign();
+		
+		return (start - System.currentTimeMillis());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private static PrivateKey generateNewKey(int keySize) {
+		KeyPairGenerator keyGen = null;
+		try {
+			keyGen = KeyPairGenerator.getInstance("RSA");
+			keyGen.initialize(keySize);
+			return keyGen.generateKeyPair().getPrivate();
+		} catch (NoSuchAlgorithmException e) {
+			Log.e(TAG, "error", e);
+			System.exit(0);
+			return null;
+		}
 	}
 
 	@Override
